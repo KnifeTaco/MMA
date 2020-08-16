b1768a72d19da37f6d041ab76f35f34eed5387f4
Andrei Liviu Georgescu <github@andreigeorgescu.com>
2020-08-16 19:59:39 -0400

    Add Contact Info

--- lib/main.dart
+++ lib/main.dart
@@ -9,39 +9,70 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.black,
-            body: SafeArea(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [Colors.lightBlue[800], Colors.white])),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
                 child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-
-                  children: <Widget>[
+                  children: [
                     CircleAvatar(
-                        radius: 50.0,
-                        backgroundImage: AssetImage("images/Douche.jpg"),
+                      radius: 100.0,
+                      backgroundImage: AssetImage("images/rishi_prasad.webp"),
                     ),
                     Text(
-                        "DICK SUCKER",
-                        style: TextStyle(
-                          fontSize: (40),
-                          color: Colors.deepOrange,
+                      "Rishi Prasad",
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Co-Owner",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    Text(
+                        "Rishi Prasad is a doctor that will diagnose your aches, sprains and poor attitudes. With more than a decade of parkour experience, he is also one of the founders of Mumbai Parkour."),
+                    SizedBox(
+                      height: 25.0,
+                      width: 150.0,
+                      child: Divider(
+                        color: Colors.black,
+                      ),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.phone,
+                          color: Colors.lightBlue[600],
                         ),
+                        title: Text("1-800"),
+                      ),
+                    ),
+                    Card(
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.mail,
+                          color: Colors.lightBlue[600],
+                        ),
+                        title: Text("email"),
+                      ),
                     ),
                   ],
                 ),
+              ),
             ),
+          ),
         ),
+      ),
     );
   }
 }
-
-/*
-Container(
-                       height: 100.0,
-                        width: 100.0,
-                        child: Image(image: AssetImage("images/Douche.jpg"),
-                        )
-                    ),
- */
