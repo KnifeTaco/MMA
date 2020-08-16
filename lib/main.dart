4257ca36a7a155e1d874edd9d01dfb9b8d75afaa
KnifeTaco <tsrivas1@binghamton.edu>
2020-08-16 18:49:42 -0400

    MMA

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,9 +10,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            backgroundColor: Colors.white,
+            backgroundColor: Colors.black,
             body: SafeArea(
-                child: Center(
-                    child: Image(image: AssetImage("images/icon.png"))))));
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+
+                  children: <Widget>[
+                    CircleAvatar(
+                        radius: 50.0,
+                        backgroundImage: AssetImage("images/Douche.jpg"),
+                    ),
+                    Text(
+                        "DICK SUCKER",
+                        style: TextStyle(
+                          fontSize: (40),
+                          color: Colors.deepOrange,
+                        ),
+                    ),
+                  ],
+                ),
+            ),
+        ),
+    );
   }
 }
+
+/*
+Container(
+                       height: 100.0,
+                        width: 100.0,
+                        child: Image(image: AssetImage("images/Douche.jpg"),
+                        )
+                    ),
+ */
